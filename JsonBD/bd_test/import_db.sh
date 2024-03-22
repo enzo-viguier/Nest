4a2cd34c5b60a1a831e7290c55e4ecc3516f8e93
enzo-viguier <enzoviguier@outlook.fr>
2024-03-22 01:43:35 +0100

    Modification du script bash

--- JsonBD/bd_test/import_db.sh
+++ JsonBD/bd_test/import_db.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Importe la collection "users" dans la base de données "NEST"
-mongoimport --db NEST --collection users --file users.json --jsonArray --drop
-
-# Importe la collection "produits" dans la base de données "NEST"
-mongoimport --db NEST --collection produits --file produits.json --jsonArray --drop
-
-# Importe la collection "marques" dans la base de données "NEST"
-mongoimport --db NEST --collection marques --file marques.json --jsonArray --drop
-
-# NEW
-
 mongoimport --db NEST --collection utilisateurs --file utilisateurs.json --jsonArray --drop
 mongoimport --db NEST --collection biens --file biens.json --jsonArray --drop
 mongoimport --db NEST --collection locations --file locations.json --jsonArray --drop
