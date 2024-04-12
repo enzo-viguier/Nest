db200f303e1c0efef6c39165e98b88b3f552906c
enzo-viguier <enzoviguier@outlook.fr>
2024-04-13 00:52:36 +0200

    Modification permettant l'affichage des biens enregistré en base de données

--- client/src/app/liste-annonce/liste-annonce.component.ts
+++ client/src/app/liste-annonce/liste-annonce.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { BiensService } from "../biens.service";
+import { HttpClientModule } from "@angular/common/http";
+import {error} from "@angular/compiler-cli/src/transformers/util";
 
 @Component({
   selector: 'app-liste-annonce',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './liste-annonce.component.html',
   styleUrl: './liste-annonce.component.css'
 })
-export class ListeAnnonceComponent {
+export class ListeAnnonceComponent implements OnInit {
+
+  biens: any[] = []
+
+  constructor(private biensService: BiensService) {}
+
+  ngOnInit(): void {
+
+    this.biensService.getBiens().subscribe({
+      next: (data) => {
+        this.biens = data;
+      },
+      error: (error) => {
+        console.log("Erreur lors de la récupération des biens:", error)
+      }
+    })
+
+  }
 
 }
