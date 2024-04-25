d33fe3db209b8406393c6aeacaf52282eaca2bfa
aurelien.milla01@etu.umontpellier.fr <aurelien.milla01@etu.umontpellier.fr>
2024-04-25 10:21:17 +0200

    ajout avis

--- client/src/app/liste-annonce/liste-annonce.component.ts
+++ client/src/app/liste-annonce/liste-annonce.component.ts
@@ -2,11 +2,12 @@ import {Component, OnInit} from '@angular/core';
 import { BiensService } from "../biens.service";
 import { HttpClientModule } from "@angular/common/http";
 import {error} from "@angular/compiler-cli/src/transformers/util";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-liste-annonce',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, JsonPipe],
   templateUrl: './liste-annonce.component.html',
   styleUrl: './liste-annonce.component.css'
 })
@@ -17,16 +18,27 @@ export class ListeAnnonceComponent implements OnInit {
   constructor(private biensService: BiensService) {}
 
   ngOnInit(): void {
-
     this.biensService.getBiens().subscribe({
-      next: (data) => {
-        this.biens = data;
+      next: (biens) => {
+        this.biens = biens; // Assuming 'biens' is an array of properties
+
+        // Iterate over each property to fetch its reviews
+        this.biens.forEach(bien => {
+          this.biensService.getAvisBienById(bien.idBien).subscribe({
+            next: (avis) => {
+              bien.avis = avis; // Assign avis to each bien
+            },
+            error: (error) => {
+              console.error("Erreur lors de la récupération des avis pour bien ID:", bien.id, error);
+              bien.avis = []; // Assign empty array on error
+            }
+          });
+        });
       },
       error: (error) => {
-        console.log("Erreur lors de la récupération des biens:", error)
+        console.error("Erreur lors de la récupération des biens:", error);
+        this.biens = []; // Set biens to an empty array on error
       }
-    })
-
+    });
   }
-
 }
