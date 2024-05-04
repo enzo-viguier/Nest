1b65e09d18c1b5e0fdf0a1d841181dee8faa4fae
enzo-viguier <enzoviguier@outlook.fr>
2024-05-05 01:17:44 +0200

    Ajout du système d'inscription

--- client/src/app/inscription/inscription.component.ts
+++ client/src/app/inscription/inscription.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {NavComponent} from "../nav/nav.component";
 import {FooterComponent} from "../footer/footer.component";
 import {RouterLink} from "@angular/router";
+import {UtilisateurService} from "../utilisateur.service";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-inscription',
@@ -9,11 +11,26 @@ import {RouterLink} from "@angular/router";
   imports: [
     NavComponent,
     FooterComponent,
-    RouterLink
+    RouterLink,
+    FormsModule
   ],
   templateUrl: './inscription.component.html',
   styleUrl: './inscription.component.css'
 })
 export class InscriptionComponent {
+
+  prenom: any;
+  nom: any;
+  email: any;
+  password: any;
+  phone: any;
+
+  constructor(private utilisateurService: UtilisateurService) {}
+
+  inscription() {
+
+    this.utilisateurService.inscription(this.nom, this.prenom, this.email, this.phone, this.password);
+
+  }
 
 }
--- client/src/app/utilisateur.service.ts
+++ client/src/app/utilisateur.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpResponse} from "@angular/common/http";
 import {Router} from "@angular/router";
+import { CookieService } from 'ngx-cookie-service';
 
 export interface ApiResponse {
   message: string;
@@ -15,6 +16,27 @@ export class UtilisateurService {
     private http: HttpClient,
     private router: Router
   ) { }
+
+  inscription(nom: string, prenom: string, mail: string, téléphone: string, password: string) {
+    console.log("Inscription de l'utilisateur avec le nom " + nom + ", le prénom " + prenom + ", le mail " + mail + " et le mot de passe " + password);
+    return this.http.post<ApiResponse>('http://localhost:8888/utilisateurs/ajouter', {mail: mail, prénom: prenom, nom: nom, téléphone: téléphone, motDePasse: password})
+      .subscribe({
+        next: (response) => {
+          // Si l'inscription est réussie
+          if (response) {
+            console.log("RESPONSE : " + response)
+            this.router.navigate(['/connexion']);  // Rediriger vers la page de connexion
+          } else {
+            console.log("ERROR INSCRIPTION")
+            this.router.navigate(['/inscription']);  // Rediriger vers la page d'inscription en cas d'échec
+          }
+        },
+        error: (error) => {
+          console.error('Erreur lors de l\'inscription', error);
+          this.router.navigate(['/inscription']);  // Rediriger vers la page d'inscription en cas d'erreur
+        }
+      });
+  }
 
   connexion(mail: string, password: string) {
     console.log("Connexion de l'utilisateur avec le mail " + mail + " et le mot de passe " + password);
